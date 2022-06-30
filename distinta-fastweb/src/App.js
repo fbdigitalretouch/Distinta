f39102a09854f5249ad61451c28fa6f3a8a64151
Federico Bottos <fb.digitalretouch@gmail.com>
2022-06-30 19:17:12 +0200

    aggiunto login-module e if per login in home page.

--- distinta-fastweb/src/App.js
+++ distinta-fastweb/src/App.js
@@ -14,30 +14,34 @@ import DistintaList from "./components/distinta-list"
 import Inserisci from "./components/Inserisci"
 import Logout from "./components/Logout"
 import Direttore from "./components/Direttore"
+import Login from "./components/login-module"
+
+const logged = false;
 
 
 function App() {
   return (
     <div className="App">
         <Router>
-            <Navigationbar />
+            <Navigationbar/>
+            {logged ? <h1>Benvenuto!</h1> : <Login/>}
             <Routes>
             <Route path="/distinta" element={<DistintaList/>} />
-            <Route path="/login" element={<Inserisci/>} />
+            <Route path="/inserisci" element={<Inserisci/>} />
             <Route path="/logout" element={<Logout/>} />
             <Route path="/direttore" element={<Direttore/>} />
+            <Route path="/login" element={<Login/>} />
             </Routes>
         </Router>
+
     </div>
     
   );
 }
 
 
-        //  
-        //  <Route path='/login' component={Login}/>
+   //      {() => {if(logged===false){
+   //           return(<Login/>)}
+   //         }}
 
-        //  <Route path='/logout' component={Logout}/>
-        //  <Route path='/direttore' component={Direttore}/>
-         
 export default App;
